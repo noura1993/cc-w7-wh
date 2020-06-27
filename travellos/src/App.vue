f36dd355d3c8186f782fb51e54f30b95bfa810c6
Nourhan Elhakim <nourhanmagdy993@gmail.com>
2020-06-27 19:10:24 +0100

    Initial wiring of SearchBar

--- travellos/src/App.vue
+++ travellos/src/App.vue
@@ -1,13 +1,32 @@
 <template>
-  
+  <div id="app">
+    <search-bar></search-bar>
+  </div>
 </template>
 
 <script>
-export default {
+import SearchBar from "./components/SearcBar";
+import { eventBus } from "./main";
 
-}
+export default {
+  name: "App",
+  data() {
+    return {
+      places: []
+    };
+  },
+
+  mounted() {
+    eventBus.$on("search-results", results => {
+      this.places = results;
+    });
+  },
+
+  components: {
+    "search-bar": SearchBar
+  }
+};
 </script>
 
 <style>
-
 </style>
--- travellos/src/components/SearcBar.vue
+++ travellos/src/components/SearcBar.vue
@@ -1,13 +1,30 @@
 <template>
-  
+  <div>
+    <input type="text" placeholder="Enter country name..." v-model="country" />
+    <button v-on:click="searchForCountry()">
+      <i class="fa fa-search"></i>
+    </button>
+  </div>
 </template>
 
 <script>
-export default {
+import { eventBus } from "../main";
 
-}
+export default {
+  name: "search-bar",
+  data() {
+    return {
+      country: ""
+    };
+  },
+
+  methods: {
+    searchForCountry: function() {
+      eventBus.$emit("search-results", [{ name: "Castle" }]);
+    }
+  }
+};
 </script>
 
 <style>
-
 </style>
