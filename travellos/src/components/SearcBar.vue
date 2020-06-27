a2d7ed8135802f91ce5773c3be86fb4ff3216336
Nourhan Elhakim <nourhanmagdy993@gmail.com>
2020-06-27 21:09:54 +0100

    Fetch country's place of interests from API

--- travellos/src/components/SearcBar.vue
+++ travellos/src/components/SearcBar.vue
@@ -14,13 +14,17 @@ export default {
   name: "search-bar",
   data() {
     return {
+      account: process.env.VUE_APP_ACCOUNT,
+      api_token: process.env.VUE_APP_API_TOKEN,
       country: ""
     };
   },
 
   methods: {
     searchForCountry: function() {
-      eventBus.$emit("search-results", [{ name: "Castle" }]);
+      fetch(`https://www.triposo.com/api/20200405/poi.json?location_id=${this.country}&count=50&account=${this.account}&token=${this.api_token}`)
+      .then(response => response.json())
+      .then(jsonResponse =>  eventBus.$emit("search-results", jsonResponse));
     }
   }
 };
